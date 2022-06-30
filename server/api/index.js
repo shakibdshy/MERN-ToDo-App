f341b494c804d86a1b46bcb3203c5b223dd68033
Shakib D shy <shakibdshy@gmail.com>
2022-07-01 05:29:25 +0600

    Update API

--- server/api/index.js
+++ server/api/index.js
@@ -1,22 +1,28 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
-const app = express()
-dotenv.config()
+import postRouter from "./routes/post.js"
+import cors from "cors";
+const app = express();
+dotenv.config();
 
-const connect = async (req, res) => {
+const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
         console.log("Connected to MongoDB")
     } catch (error) {
         throw error
-
     }
 }
 
 mongoose.connection.on("Disconnected", () => {
     console.log("Disconnected from MongoDB")
 })
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/post", postRouter);
 
 app.listen(5000, () => {
     connect()
---
+++ server/api/routes/post.js
@@ -0,0 +1,53 @@
+import express from "express";
+import Post from "../models/Post.js";
+
+const router = express.Router();
+
+// Create
+router.post('/', async (req, res) => {
+    const newPost = new Post(req.body)
+    try {
+        const savePost = await newPost.save();
+        res.status(200).json(savePost);
+    } catch (error) {
+        res.status(500).json(err);
+    }
+});
+// Update
+router.put('/:id', async (req, res) => {
+    try {
+        const UpdatePost = await Post.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(UpdatePost);
+    } catch (error) {
+        res.status(500).json(err)
+    }
+});
+// Delete
+router.delete('/:id', async (req, res) => {
+    try {
+        const UpdatePost = await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json("Post deleted");
+    } catch (error) {
+        res.status(500).json(err)
+    }
+});
+// Get
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(err)
+    }
+});
+// Get All
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json(err)
+    }
+});
+
+export default router
