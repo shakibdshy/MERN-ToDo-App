fe41f33d9fa5b963d15284897301f27f217bb313
Shakib D shy <shakibdshy@gmail.com>
2022-07-01 03:00:21 +0600

    Update Todo List

--- client/components/TodoList.js
+++ client/components/TodoList.js
@@ -1,40 +1,91 @@
-import { Checkbox, IconButton, Typography } from '@material-tailwind/react'
+import { Button, Checkbox, IconButton, Input, Typography } from '@material-tailwind/react'
 import React, { useState } from 'react'
 import { MdDelete, MdOutlineSystemUpdateAlt } from 'react-icons/md'
 import { useStateContext } from '../context/StateContext'
 
 function TodoList() {
     const [todos, setTodos] = useStateContext()
-    // console.log(todos);
+    const [edit, setEdit] = useState(false)
+    const [editValue, setEditValue] = useState(todos.name)
+    console.log(edit);
 
     const checkComplete = (id) => {
         const newTodos = [...todos]
         newTodos[id].complete = !newTodos[id].complete
         setTodos(newTodos)
-        console.log(newTodos[id]);
+    }
+
+    const handleEdit = () => {
+        setEdit(true)
+    }
+
+    const handleEditTodos = (editValue, id) => {
+        const newTodos = [...todos]
+        newTodos[id].name = editValue
+        setTodos(newTodos)
+    }
+
+    const handleUpdate = (id) => {
+        setEdit(false)
+        if (setEdit) {
+            handleEditTodos(editValue, id)
+        } else {
+            setEdit(todos.name)
+        }
+    }
+
+    const handleDelete = (id) => {
+        const newTodos = [...todos]
+        newTodos.splice(id, 1)
+        setTodos(newTodos)
     }
 
     return (
         <>
             <ul className='flex flex-col gap-4'>
                 {
-                    todos.map((todo, index) => (
-                        <li key={index} className={`flex items-center justify-between gap-4 text-left bg-white rounded-lg py-2 px-4 pl-2 ${todo.complete ? 'complete' : 'check'}`}>
-                            <div className='flex items-center gap-2'>
-                                <Checkbox id={index} color="teal" onChange={() => checkComplete(index)} />
-                                <Typography>{todo.name}</Typography>
-                            </div>
-                            <div className='flex items-center gap-2'>
-                                <IconButton color="blue">
-                                    <MdOutlineSystemUpdateAlt className='text-xl' />
-                                </IconButton>
-                                <IconButton color="red">
-                                    <MdDelete className='text-xl' />
-                                </IconButton>
-                            </div>
-                        </li>
+                    todos.map((todo, index) =>
+                        edit ? (
+                            <li key={index} className={`flex items-center justify-between gap-4 text-left bg-white rounded-lg py-2 px-4 pl-2 ${todo.complete ? 'complete' : 'check'}`}>
+                                <div className='flex items-center gap-2 w-full'>
+                                    <Input
+                                        type='text'
+                                        size="lg"
+                                        variant="standard"
+                                        value={editValue}
+                                        name='editValue'
+                                        onChange={(e) => setEditValue(e.target.value)}
+                                    />
+                                    <IconButton className='basis-auto grow-0 shrink-0' color="green" onClick={() => handleUpdate(index)}>
+                                        <MdOutlineSystemUpdateAlt className='text-xl' />
+                                    </IconButton>
+                                </div>
+                            </li>
+                        ) : (
+                            <li key={index} className={`flex items-center justify-between gap-4 text-left bg-white rounded-lg py-2 px-4 pl-2 ${todo.complete ? 'complete' : 'check'}`}>
+                                <div className='flex items-center gap-2'>
+                                    <Checkbox id={index} color="teal" onChange={() => checkComplete(index)} />
+                                    {
+                                        todo.complete ? <del>{todo.name}</del> : <Typography>{todo.name}</Typography>
 
-                    ))
+                                    }
+                                    {
+                                        console.log(todo.complete)
+                                    }
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    {
+                                        todo.complete ? '' : <IconButton color="blue" onClick={handleEdit}>
+                                            <MdOutlineSystemUpdateAlt className='text-xl' />
+                                        </IconButton>
+                                    }
+                                    <IconButton color="red" onClick={() => handleDelete(index)}>
+                                        <MdDelete className='text-xl' />
+                                    </IconButton>
+                                </div>
+                            </li>
+                        )
+                    )
                 }
             </ul>
         </>
--- client/pages/complete.js
+++ client/pages/complete.js
@@ -1,11 +1,20 @@
+import { Checkbox, IconButton, Typography } from '@material-tailwind/react'
 import Head from 'next/head'
 import React from 'react'
 import Header from '../components/Header'
 import Todo from '../components/Todo'
-import useStateContext from '../context/StateContext'
+import { useStateContext } from '../context/StateContext'
+import { MdDelete, MdOutlineSystemUpdateAlt } from 'react-icons/md'
 
 function complete() {
-    const [list, setList] = useStateContext()
+    const [todos, setTodos] = useStateContext();
+    const completed = todos.filter(todo => todo.complete);
+    
+    const handleDelete = (id) => { 
+        const newTodos = [...todos]
+        newTodos.splice(id, 1)
+        setTodos(newTodos)
+    }
 
     return (
         <>
@@ -17,10 +26,26 @@ function complete() {
             <Header />
 
             <section className='py-24 text-center container mx-auto px-4'>
-                <div className="container mx-auto px-4">
-                    {
-                        list.length
-                    }
+                <div className="container max-w-xl mx-auto px-4">
+                    <Typography variant='h1'>Completed Task List</Typography>
+                    <ul className='flex flex-col gap-4 mt-8'>
+                        {
+                            completed.map((todo, index) => (
+                                <li key={index} className={`flex items-center justify-between gap-4 text-left bg-white rounded-lg py-2 px-4 pl-2`}>
+                                    <div className='flex items-center gap-2'>
+                                        <Checkbox id={index} color="green" defaultChecked />
+                                        <del>{todo.name}</del>
+                                    </div>
+                                    <div className='flex items-center gap-2'>
+                                        <IconButton color="red" onClick={() => handleDelete(index)}>
+                                            <MdDelete className='text-xl' />
+                                        </IconButton>
+                                    </div>
+                                </li>
+
+                            ))
+                        }
+                    </ul>
                 </div>
             </section>
         </>
