cc930bede23fde49df8fb34735b61faf280b355f
Shakib D shy <shakibdshy@gmail.com>
2022-07-01 10:55:01 +0600

    Update

--- server/api/models/Post.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-const PostSchema = new Schema({
-    name: String,
-    complete: Boolean,
-});
-
-export default mongoose.model('Post', PostSchema);
